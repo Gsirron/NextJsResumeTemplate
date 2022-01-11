a7dfe9625a2b887fcc690c4c86dfaf74f7ce2b27
Norris Luong <33255034+Gsirron@users.noreply.github.com>
2022-01-12 00:28:38 +1100

    Added Aboutpage using grid

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,35 +1,71 @@
 import { ReactNode } from "react";
 
 import {
-	Stack,
-	Flex,
-	Button,
-	Text,
-	VStack,
-	useBreakpointValue,
-} from '@chakra-ui/react';
+  Stack,
+  Flex,
+  Button,
+  Text,
+  VStack,
+  useBreakpointValue,
+  Container,
+  Box,
+  Heading,
+  SimpleGrid,
+  Grid,
+  GridItem,
+} from "@chakra-ui/react";
 
-import React from 'react'
+import React from "react";
 
 const About = () => {
-	return (
-		<Flex
-		w={'full'}
-		h={'100vh'}
-		backgroundSize={'cover'}
-		backgroundPosition={'center center'}
-		>
-			<VStack
-			w={'full'}
-				justify={'center'}
-			>
-				<Text>
-					About
-				</Text>
-			</VStack>
+  return (
+    <Box bg={"gray.800"} position={"relative"}>
+      <Flex
+        flex={1}
+        zIndex={0}
+        display={{ base: "none", lg: "flex" }}
+        backgroundSize={"cover"}
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+        position={"absolute"}
+        width={"50%"}
+        insetY={0}
+        right={0}
+      >
+        <Flex
+          bgGradient={"linear(to-r, gray.800 10%, transparent)"}
+          w={"full"}
+          h={"full"}
+        />
+      </Flex>
+      <Container maxW={"4xl"} zIndex={2} position={"relative"}>
+        <Grid
+          m={{ base: 8, md: 20 }}
+          h="200px"
+          templateRows="repeat(2, 1fr)"
+          templateColumns="repeat(3, 1fr)"
+          gap={{ base: 4, md: 10, xl: 10 }}
+        >
+          <GridItem rowSpan={2} colSpan={1}></GridItem>
+          <GridItem colSpan={2}>
+            <Heading
+              color={"white"}
+              mb={4}
+              fontSize={{ base: "2xl", md: "4xl" }}
+            >
+              About Me
+            </Heading>
+            <Text fontSize={"xl"} color={"gray.400"}>
+              The NewLife™ technology allows you to monitor your crops and get
+              complete insights at real time. The proprietary software/hardware
+              ecosystem prevents your plants from getting neglected.
+            </Text>
+          </GridItem>
+          <GridItem colSpan={2}></GridItem>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
 
-		</Flex>
-	)
-}
-
-export default About
+export default About;
--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,32 +1,52 @@
-import React from 'react'
+import React from "react";
 import {
-	Stack,
-	Flex,
-	Button,
-	Text,
-	VStack,
-	useBreakpointValue,
-} from '@chakra-ui/react';
+  Stack,
+  Flex,
+  Button,
+  Text,
+  VStack,
+  useBreakpointValue,
+  Heading,
+} from "@chakra-ui/react";
 
 const Home = () => {
-	return (
-		<Flex
-			w={'full'}
-			h={'100vh'}
-			backgroundSize={'cover'}
-			backgroundPosition={'center center'}
-		>
-			<VStack
-				w={'full'}
-				justify={'center'}
-			>
-				<Text>
-					Home
-				</Text>
-			</VStack>
+  return (
+    <Flex
+      w={"full"}
+      h={"100vh"}
+      flex={1}
+      backgroundSize={"cover"}
+      backgroundPosition={"center center"}
+      backgroundImage={"/images/frontbg2.jpg"}
+    >
+      <VStack
+        w={"full"}
+        justify={"center"}
+        px={useBreakpointValue({ base: 4, md: 8 })}
+        bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
+      >
+        <Stack maxW={"2xl"} align={"center"} spacing={6}>
+          <Heading
+            alignContent={"center"}
+            color={"white"}
+            fontWeight={700}
+            lineHeight={1.2}
+            fontSize={useBreakpointValue({ base: "4xl", md: "6xl" })}
+          >
+            I'm Norris Luong
+          </Heading>
+          <Text
+            color={"white"}
+            fontWeight={700}
+            lineHeight={1.2}
+            fontSize={useBreakpointValue({ base: "2xl", md: "3xl" })}
+          >
+            I'm a full stack web devloper based in Sydney Australia.
+          </Text>
+        </Stack>
+      </VStack>
+    </Flex>
+  );
+};
 
-		</Flex>
-	)
-}
-
-export default Home
+export default Home;
